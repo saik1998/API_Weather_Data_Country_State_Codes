ad5fda3dd6cd4039dd4ffbfa077d2c1518daf155
saik1998 <107240573+saik1998@users.noreply.github.com>
2022-10-31 17:34:01 +0530

    Update commit

--- app/src/main/java/com/firstapp/api_weather_data_country_state_codes/MainActivity.java
+++ app/src/main/java/com/firstapp/api_weather_data_country_state_codes/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,111 +29,138 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView1;
     EditText cityname1;
+    Button submit1;
 
 
     ProgressDialog progressDialog;
     RequestQueue requestQueue;
     String key;
 
-    String cityUrl,cityStr;
-
+    String cityUrl, cityStr;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView1=findViewById(R.id.text_info);
-        cityname1=findViewById(R.id.cityEdt);
+        textView1 = findViewById(R.id.text_info);
+        cityname1 = findViewById(R.id.cityEdt);
 
-        requestQueue= Volley.newRequestQueue(this);
-        progressDialog=new ProgressDialog(this);
+        requestQueue = Volley.newRequestQueue(this);
+        progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
+        submit1 = findViewById(R.id.submit);
 
 
-    }
 
-    private void dataMethod()
-    {
-        cityUrl="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-    }
-
-    public void cityname(View view) {
-        progressDialog.show();
-        cityStr=cityname1.getText().toString();
-        key=getResources().getString(R.string.weather_key);
-
-        cityUrl="https://api.openweathermap.org/data/2.5/weather?q="+cityStr+"&appid="+key;
-
-
-        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.POST, cityUrl, null, new Response.Listener<JSONObject>() {
+        submit1.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onResponse(JSONObject response) {
-                progressDialog.dismiss();
+            public void onClick(View view)
+            {
 
-                try {
+                String cityStr = cityname1.getText().toString().trim();
 
 
-                    JSONObject jsonObject=response.getJSONObject("coord");
-                    double lon= jsonObject.getDouble("lon");
-                    double lat= jsonObject.getDouble("lat");
+                if (TextUtils.isEmpty(cityStr)) {
 
-                    JSONArray jsonArray=response.getJSONArray("weather");
-                    JSONObject weatherObj=jsonArray.getJSONObject(0);
-
-                    int id=weatherObj.getInt("id");
-                    String main=weatherObj.getString("main");
-                    String description=weatherObj.getString("description");
-
-                    long visibility=response.getLong("visibility");
-
-                    JSONObject wind=response.getJSONObject("wind");
-                    double speed=wind.getDouble("speed");
-                    //  double gust=wind.getDouble("gust");
-
-                    String name=response.getString("name");
-
-                    textView1.setText(""+lat+"\n"+lon+"\n"+id+
-                            "\n"+main+"\n"+description+"\n"+visibility+"\n"+speed+"\n"+name);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-
-                    Toast.makeText(MainActivity.this, "kk"+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    cityname1.setError("Enter Name");
+                }
+                if (cityStr.length() == 0) {
+                    cityname1.setError(("Field cannot be Empty"));
+                } else
+                {
+                    Toast.makeText(MainActivity.this, "" + cityStr, Toast.LENGTH_LONG).show();
                 }
 
 
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                progressDialog.dismiss();
-                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
 
-                    Toast.makeText(getApplicationContext(), "Time out or no connection  error", Toast.LENGTH_SHORT).show();
-                } else if (error instanceof AuthFailureError) {
-                    Toast.makeText(getApplicationContext(), "AuthFailureError error", Toast.LENGTH_SHORT).show();
+             progressDialog.show();
+            cityStr=cityname1.getText().toString();
+            key=getResources().getString(R.string.weather_key);
 
-                } else if (error instanceof ServerError) {
-                    Toast.makeText(getApplicationContext(), "ServerError error", Toast.LENGTH_SHORT).show();
+            cityUrl="https://api.openweathermap.org/data/2.5/weather?q="+cityStr+"&appid="+key;
 
-                } else if (error instanceof NetworkError) {
-                    Toast.makeText(getApplicationContext(), "NetworkError error", Toast.LENGTH_SHORT).show();
 
-                } else if (error instanceof ParseError) {
-                    Toast.makeText(getApplicationContext(), "ParseError error", Toast.LENGTH_SHORT).show();
+            JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.POST, cityUrl, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    progressDialog.dismiss();
+
+                    try {
+
+
+                        JSONObject jsonObject=response.getJSONObject("coord");
+                        double lon= jsonObject.getDouble("lon");
+                        double lat= jsonObject.getDouble("lat");
+
+                        JSONArray jsonArray=response.getJSONArray("weather");
+                        JSONObject weatherObj=jsonArray.getJSONObject(0);
+
+                        int id=weatherObj.getInt("id");
+                        String main=weatherObj.getString("main");
+                        String description=weatherObj.getString("description");
+
+                        long visibility=response.getLong("visibility");
+
+                        JSONObject wind=response.getJSONObject("wind");
+                        double speed=wind.getDouble("speed");
+                        //  double gust=wind.getDouble("gust");
+
+                        String name=response.getString("name");
+
+                        textView1.setText(""+lat+"\n"+lon+"\n"+id+
+                                "\n"+main+"\n"+description+"\n"+visibility+"\n"+speed+"\n"+name);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+
+                        Toast.makeText(MainActivity.this, "kk"+e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
 
                 }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    if (error instanceof TimeoutError || error instanceof NoConnectionError) {
 
-            }
-        });
+                        Toast.makeText(getApplicationContext(), "Time out or no connection  error", Toast.LENGTH_SHORT).show();
+                    } else if (error instanceof AuthFailureError) {
+                        Toast.makeText(getApplicationContext(), "AuthFailureError error", Toast.LENGTH_SHORT).show();
+
+                    } else if (error instanceof ServerError) {
+                        Toast.makeText(getApplicationContext(), "ServerError error", Toast.LENGTH_SHORT).show();
+
+                    } else if (error instanceof NetworkError) {
+                        Toast.makeText(getApplicationContext(), "NetworkError error", Toast.LENGTH_SHORT).show();
+
+                    } else if (error instanceof ParseError) {
+                        Toast.makeText(getApplicationContext(), "ParseError error", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
 
         requestQueue.add(jsonObjectRequest);
+        }
+        });
     }
+
+    private void dataMethod() {
+        cityUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+
+    }
+
 
     public void nextPage(View view) {
         Intent intent=new Intent(MainActivity.this,CityCodeActivity.class);
